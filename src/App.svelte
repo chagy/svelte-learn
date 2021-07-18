cdd4155f518b59f981395d3ecd290590b14687cb
chagy <chagy_x_@hotmail.com>
2021-07-18 20:37:36 +0700

    36 understanding event modifers

--- src/App.svelte
+++ src/App.svelte
@@ -9,6 +9,7 @@
   let createdContacts = [];
 
   function addContact() {
+    // event.preventDefault();
     if (
       name.trim().length == 0 ||
       title.trim().length == 0 ||
@@ -40,26 +41,30 @@
   }
 </script>
 
-<div id="form">
-  <div class="form-control">
-    <label for="userName">User Name</label>
-    <input type="text" bind:value={name} id="userName" />
+<form id="form">
+  <div id="form">
+    <div class="form-control">
+      <label for="userName">User Name</label>
+      <input type="text" bind:value={name} id="userName" />
+    </div>
+    <div class="form-control">
+      <label for="jobTitle">Job Title</label>
+      <input type="text" bind:value={title} id="jobTitle" />
+    </div>
+    <div class="form-control">
+      <label for="image">Image URL</label>
+      <input type="text" bind:value={image} id="image" />
+    </div>
+    <div class="form-control">
+      <label for="desc">Description</label>
+      <textarea rows="3" bind:value={description} id="desc" />
+    </div>
   </div>
-  <div class="form-control">
-    <label for="jobTitle">Job Title</label>
-    <input type="text" bind:value={title} id="jobTitle" />
-  </div>
-  <div class="form-control">
-    <label for="image">Image URL</label>
-    <input type="text" bind:value={image} id="image" />
-  </div>
-  <div class="form-control">
-    <label for="desc">Description</label>
-    <textarea rows="3" bind:value={description} id="desc" />
-  </div>
-</div>
 
-<button on:click={addContact}>Add Contact Card</button>
+  <button on:click|preventDefault={addContact} type="submit"
+    >Add Contact Card</button
+  >
+</form>
 <button on:click={deleteFirst}>Delete First</button>
 <button on:click={deleteLast}>Delete Last</button>
 
@@ -85,5 +90,6 @@
   #form {
     width: 30rem;
     max-width: 100%;
+    margin: 1rem 0px;
   }
 </style>
