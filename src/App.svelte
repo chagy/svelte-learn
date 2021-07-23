a9df93beb3c2e569e8796c49d6ef6a7858a53566
chagy <chagy_x_@hotmail.com>
2021-07-23 20:24:54 +0700

    85

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,17 @@
   import Toggle from "./Toggle.svelte";
 
   let val = "Max";
+  let price = 0;
   let selectedOption = 1;
+  let agreed;
+  // let favColor = "red";
+  let favColor = ["red"];
 
   $: console.log(val);
   $: console.log(selectedOption);
+  $: console.log(price);
+  $: console.log(agreed);
+  $: console.log(favColor);
 
   function setValue(event) {
     val = event.target.value;
@@ -19,3 +26,26 @@
 <CustomInput bind:val />
 
 <Toggle bind:chosenOption={selectedOption} />
+
+<input type="number" bind:value={price} />
+
+<label
+  >Agree to terms?
+  <input type="checkbox" bind:checked={agreed} />
+</label>
+
+<h1>Color</h1>
+<label>
+  <input type="checkbox" value="red" name="color" bind:group={favColor} />
+  Red
+</label>
+
+<label>
+  <input type="checkbox" value="green" name="color" bind:group={favColor} />
+  green
+</label>
+
+<label>
+  <input type="checkbox" value="blue" name="color" bind:group={favColor} />
+  blue
+</label>
