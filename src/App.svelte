f42bf9fd44bf2c84943656044d99cedd28931bf5
chagy <chagy_x_@hotmail.com>
2021-07-28 19:41:01 +0700

    150

--- src/App.svelte
+++ src/App.svelte
@@ -1,64 +1,90 @@
 <script>
-  import meetups from "./Meetups/meetups-store.js";
-  import Header from "./UI/Header.svelte";
-  import MeetupGrid from "./Meetups/MeetupGrid.svelte";
-  import TextInput from "./UI/TextInput.svelte";
-  import Button from "./UI/Button.svelte";
-  import EditMeetup from "./Meetups/EditMeetup.svelte";
-  import MeetupDetail from "./Meetups/MeetupDetail.svelte";
+  import { onMount } from "svelte";
+  import { get } from "svelte/store";
+  import hobbyStore from "./hobby-store";
 
-  // let meetups = ;
+  let hobbies = [];
+  let hobbyInput;
+  let isLoading = false;
 
-  let editMode;
-  let editedId;
-  let page = "overview";
-  let pageData = {};
+  //   onMount(() => {
+  isLoading = true;
+  let getHobbies = fetch(
+    "https://javascript-4d383-default-rtdb.asia-southeast1.firebasedatabase.app/hobbies.json"
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Failed!");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      isLoading = false;
+      //   hobbies = Object.values(data);
+      hobbyStore.setHobbies(Object.values(data));
+      let keys = Object.keys(data);
+      console.log(keys);
 
-  function savedMeetup(event) {
-    editMode = null;
-    editedId = null;
-  }
+      for (const key in data) {
+        console.log(key, data[key]);
+      }
 
-  function cancelEdit() {
-    editMode = null;
-    editedId = null;
-  }
+      return hobbies;
+    })
+    .catch((err) => {
+      isLoading = false;
+      console.log(err);
+    });
+  //   });
 
-  function showDetails(event) {
-    page = "details";
-    pageData.id = event.detail;
-  }
-
-  function closeDetails() {
-    page = "overview";
-    pageData = {};
-  }
-
-  function startEdit(event) {
-    editMode = "edit";
-    editedId = event.detail;
+  function addHobby() {
+    hobbyStore.addHobby(hobbyInput.value);
+    isLoading = true;
+    fetch(
+      "https://javascript-4d383-default-rtdb.asia-southeast1.firebasedatabase.app/hobbies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(hobbyInput.value),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        isLoading = false;
+        if (!res.ok) {
+          throw new Error("Failed!");
+        }
+        alert("DATA SAVE");
+      })
+      .catch((err) => {
+        isLoading = false;
+        console.log(err);
+      });
   }
 </script>
 
-<style>
-  main {
-    margin-top: 5rem;
-  }
-</style>
+<label for="hobby">Hobby</label>
+<input type="text" name="hobby" id="hobby" bind:this={hobbyInput} />
+<button on:click={addHobby}>Add Hobby</button>
+{#if isLoading}
+  <p>Loading...</p>
+{:else}
+  <ul>
+    {#each $hobbyStore as hobby}
+      <li>{hobby}</li>
+    {/each}
+  </ul>
+{/if}
 
-<Header />
-
-<main>
-  {#if page === 'overview'}
-    {#if editMode === 'edit'}
-      <EditMeetup id={editedId} on:save={savedMeetup} on:cancel={cancelEdit} />
-    {/if}
-    <MeetupGrid
-      meetups={$meetups}
-      on:showdetails={showDetails}
-      on:edit={startEdit}
-      on:add={() => {editMode = 'edit'}} />
-  {:else}
-    <MeetupDetail id={pageData.id} on:close={closeDetails} />
-  {/if}
-</main>
+<!-- {#await getHobbies}
+  <p>Loading...</p>
+{:then hobbyData}
+  <ul>
+    {#each hobbyData as hobby}
+      <li>{hobby}</li>
+    {/each}
+  </ul>
+{:catch error}
+  <p>{error.message}</p>
+{/await} -->
